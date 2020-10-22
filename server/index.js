7259fabe7ed0e78c15bb20f6f4e015c2273909ba
Milosz Czapla <totmelek@gmail.com>
2020-10-22 16:51:28 +0200

    added static assets and frond made

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const posts = require("./routes/api/lessons");
+
+if (process.env.NODE_ENV === "production") {
+  //static folder
+  app.use(express.static(__dirname + "/public"));
+
+  app.get(/.*/, (req,res) => res.sendFile(__dirname + '/puvlic/index.html'));
+}
+
 app.use("/api/lessons", posts);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
